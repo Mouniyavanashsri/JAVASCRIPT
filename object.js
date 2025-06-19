3f68ce0950427a47267588c0b02c3fa765b902f5
Mouniyavanashsri <mouniyavanashsrimurugan@gmail.com>
2025-06-19 14:40:08 +0530

    complete objects in js

---
+++ object.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  console.log(a + b);
+}
+
+let newObject = {
+  name: "John",
+  age: 25,
+  func: (a, b) => {
+    console.log(a + b);
+  },
+};
+console.log(newObject.name);
+console.log(newObject.age);
+newObject.func(2, 3);
