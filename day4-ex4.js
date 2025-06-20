8c98754ebad3709e000c4b38b169bfd5b3f24503
Mouniyavanashsri <mouniyavanashsrimurugan@gmail.com>
2025-06-20 12:08:53 +0530

    complete task-day4 in js

---
+++ day4-ex4.js
@@ -0,0 +1,5 @@
+// 5a. Create a variable called 'name' and store your name
+const name = "Mouniyavanashsri";
+
+// 5b. Display the message using template literals
+console.log(`My name is: ${name}`);
